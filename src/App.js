91914dfde46adea9d5f5a834d2393f359bc0d765
salamat2201 <daribaev_14@icloud.com>
2024-12-22 13:50:09 +0500

    fix router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+
+
+
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
@@ -16,6 +19,8 @@ import Aesthetic from './components/Aesthetic';
 import FAQ from './components/FAQ';
 import ContactCards from './components/ContactCards';
 
+
+
 const App = () => {
   return (
     <Router>
@@ -24,30 +29,30 @@ const App = () => {
           <Route
             path="/"
             element={
-            <>
-              <div className='back'>
-              <Navbar />
-              <Home />
-              </div>
-              <div className="divider"></div>
-              <About />
-              <ParallaxSection />
-              <ConsultationSection />
-              <ExpandingImageSection />
-              <CapsuleFeatures />
-              <CapsuleBusinessInfo />
-              <div id="profitability">
-              {/* <Profitability /> */}
-              </div>
-              <div id="catalog">
-              <Catalog />
-              </div>
-              <Features />
-              <ConsultationSection/>
-              <Aesthetic/>
-              <FAQ/>
-              <div id="contact" ><ContactCards /></div>              
-            </>
+              <>
+                <div className='back'>
+                <Navbar />
+                <Home />
+                </div>
+                <div className="divider"></div>
+                <About />
+                <ParallaxSection />
+                <ConsultationSection />
+                <ExpandingImageSection />
+                <CapsuleFeatures />
+                <CapsuleBusinessInfo />
+                <div id="profitability">
+                {/* <Profitability /> */}
+                </div>
+                <div id="catalog">
+                <Catalog />
+                </div>
+                <Features />
+                <ConsultationSection/>
+                <Aesthetic/>
+                <FAQ/>
+                <div id="contact" ><ContactCards /></div>              
+              </>
             }
           />
           <Route path="/:id" element={<HouseDetails />} />
